a58370ccc9505735425146d2d021dcb9b95d8080
John Koszarek <john.koszarek@gmail.com>
2023-02-24 10:24:39 -0600

    Move to util folder and use Google-style function names.

--- util/math.h
+++ util/math.h
@@ -4,16 +4,16 @@
 
 const double PI = 3.1415926535897932385;
 
-inline double degrees_to_radians(double degrees) {
+inline double DegreesToRadians(double degrees) {
   return degrees * PI / 180.0;
 }
 
 // Returns a random double in [0, 1).
-inline double random_double() {
+inline double RandomDouble() {
   return rand() / (RAND_MAX + 1.0);
 }
 
 // Returns a random double in [min, max).
-inline double random_double(double min, double max) {
-  return min + (max - min) * random_double();
+inline double RandomDouble(double min, double max) {
+  return min + (max - min) * RandomDouble();
 }
