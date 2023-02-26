ef1051c56a4575a618442e20257e8ee7fa0cd5f5
John Koszarek <john.koszarek@gmail.com>
2023-02-26 11:46:39 -0600

    Moved math-related constants to this file.

--- util/math.h
+++ util/math.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #include <cstdlib>
+#include <limits>
 
 const double PI = 3.1415926535897932385;
+
+const double MIN_T = 0.001;
+const double MAX_T = std::numeric_limits<double>::infinity();
+
 
 inline double DegreesToRadians(double degrees) {
   return degrees * PI / 180.0;
