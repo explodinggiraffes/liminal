2a3bfd773e53e22f3ee5663f317e5b5af5ce5635
John Koszarek <john.koszarek@gmail.com>
2023-02-28 16:46:14 -0600

    Remove constant and function only being used by the Camera class.

--- util/math.h
+++ util/math.h
@@ -3,22 +3,15 @@
 #include <cstdlib>
 #include <limits>
 
-const double PI = 3.1415926535897932385;
-
 const double MIN_T = 0.001;
 const double MAX_T = std::numeric_limits<double>::infinity();
 
-
-inline double DegreesToRadians(double degrees) {
-  return degrees * PI / 180.0;
-}
-
-// Returns a random double in [0, 1).
+/* Returns a random double in[0, 1). */
 inline double RandomDouble() {
   return rand() / (RAND_MAX + 1.0);
 }
 
-// Returns a random double in [min, max).
+/* Returns a random double in [min, max). */
 inline double RandomDouble(double min, double max) {
   return min + (max - min) * RandomDouble();
 }
