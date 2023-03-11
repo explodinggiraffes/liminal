9cceed25cf000726fae690a6bf15ab711652a626
John Koszarek <john.koszarek@gmail.com>
2023-03-11 16:28:56 -0600

    Rnamed class HittableObjects to Scene. Also renamed its header and implementation files to match the new class name.

---
+++ scene.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "hittable.h"
+
+#include <memory>
+#include <vector>
+
+class Scene {
+public:
+  void Add(std::unique_ptr<Hittable> hittable);
+
+  void Clear();
+
+  bool Hit(const Ray& ray, double t_min, double t_max, HittableProperties& properties) const;
+
+private:
+  std::vector<std::unique_ptr<Hittable>> hittable_objects_;
+};
